de863febbff3a28bcdea83911c252f00811c7b2e
vinitecnico <vinitecnico@gmail.com>
2023-10-28 15:54:19 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def query_records():
+    name = request.args.get('name')
+    with open('test.json') as f:
+        data = json.load(f)
+    print(data)
+    if data and name:
+        return jsonify(data[name])
+    elif data:
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'data not found'})
+
+# @app.route('/', methods=['PUT'])
+# def create_record():
+#     record = json.loads(request.data)
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#     if not data:
+#         records = [record]
+#     else:
+#         records = json.loads(data)
+#         records.append(record)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(records, indent=2))
+#     return jsonify(record)
+
+# @app.route('/', methods=['POST'])
+# def update_record():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#     for r in records:
+#         if r['name'] == record['name']:
+#             r['email'] = record['email']
+#         new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
+    
+# @app.route('/', methods=['DELETE'])
+# def delte_record():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#         for r in records:
+#             if r['name'] == record['name']:
+#                 continue
+#             new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
+
+app.run(debug=True)
